c3c1f7ac5b989f440bdc237e80184fbb288ddded
lrembacz <qwester1213@op.pl>
2019-09-20 20:53:15 +0200

    @dragndrop/draggable and @dragndrop/dropzone to dragndrop

--- packages/dragndrop/index.ts
+++ packages/dragndrop/index.ts
@@ -1,5 +1,5 @@
-import * as draggable from '../draggable/index';
-import * as dropzone from '../dropzone/index';
+import * as draggable from '@dragndrop/draggable/index';
+import * as dropzone from '@dragndrop/dropzone/index';
 
 export {
     draggable,
